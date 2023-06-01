b40f6be5b79cb4abcfc8340ae33105e8f51e546f
Rutger1111 <djrutgervandelaar@gmail.com>
2023-06-01 09:11:36 +0200

    lookaround
    lookaround

---
+++ FakeNewsNetwork/Assets/Scripts/EnvirementInteraction.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnvirementInteraction : MonoBehaviour
+{
+    private Ray ray;
+    private RaycastHit hit;
+    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private float positionSpeed;
+    [SerializeField] private float rotationSpeed;
+
+    [SerializeField] private bool _isZooming = false;
+    [SerializeField] private bool _isZoomingOut = false;
+    [SerializeField] public bool _isZoomedIn = false;
+
+    [SerializeField] private Vector3 cameraCloseInPosition;
+    [SerializeField] private Vector3 cameraCloseOutPosition;
+
+    [SerializeField] private Vector3 cameraCloseInLookRotation;
+    [SerializeField] private Vector3 cameraCloseOutLookRotation;
+
+    [SerializeField] private Vector3 cameraCloseOutLookDirection;
+    [SerializeField] private Vector3 cameraCloseOutDirection;
+
+    [SerializeField] private Vector3 closeInDirection;
+    [SerializeField] private Vector3 closeInLookDirection;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward*Mathf.Infinity);
+        ray = (Camera.main.ScreenPointToRay(Input.mousePosition));
+        if (Input.GetKeyDown(KeyCode.Mouse0) && _isZoomedIn == false)
+        {
+            Zoom(true);
+        }
+        if(_isZooming == true)
+        {
+            ZoomingIn();
+        }
+        if (_isZoomedIn == true || _isZoomingOut == true)
+        {
+            ZoomingOut();
+            if (Input.GetMouseButtonDown(1) && _isZooming == false)
+            {
+                _isZoomingOut = true;
+                ZoomingOut();
+            }
+        }
+    }
+    public void ZoomingIn()
+    {
+        closeInDirection = Vector3.Normalize(Camera.main.transform.position - cameraCloseInPosition);
+        closeInLookDirection = Vector3.Normalize(Camera.main.transform.eulerAngles - cameraCloseInLookRotation);
+        Debug.Log(closeInDirection);
+        Camera.main.transform.position -= (closeInDirection * positionSpeed) * Time.deltaTime;
+        Camera.main.transform.eulerAngles -= (closeInLookDirection * rotationSpeed) * Time.deltaTime;
+        if (Vector3.Distance(Camera.main.transform.position, cameraCloseInPosition) <= 0.003)
+        {
+            _isZooming = false;
+            Camera.main.transform.position = cameraCloseInPosition;
+            Camera.main.transform.eulerAngles = cameraCloseInLookRotation;
+        }
+    }
+    public void ZoomingOut()
+    {
+        cameraCloseOutDirection = Vector3.Normalize(Camera.main.transform.position - cameraCloseOutPosition);
+        cameraCloseOutLookDirection = Vector3.Normalize(Camera.main.transform.eulerAngles - cameraCloseOutLookRotation);
+        Debug.Log(closeInDirection);
+        Camera.main.transform.position -= (cameraCloseOutDirection * positionSpeed) * Time.deltaTime;
+        Camera.main.transform.eulerAngles -= (cameraCloseOutLookDirection * rotationSpeed) * Time.deltaTime;
+        if (Camera.main.transform.position.z < -1.53)
+        {
+            _isZoomingOut = false;
+            _isZoomedIn = false;
+            Camera.main.transform.position = cameraCloseOutPosition;
+            Camera.main.transform.eulerAngles = cameraCloseOutLookRotation;
+        }
+    }
+    public void Zoom(bool zoomIn)
+    {
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+        {
+            if (hit.collider.tag == "PC" && _isZooming == false)
+            {
+                _isZooming = true;
+                _isZoomedIn = true;
+                Debug.Log("did hit");
+            }
+        }
+    }
+}
--- FakeNewsNetwork/Assets/Scripts/LookAround.cs
+++ FakeNewsNetwork/Assets/Scripts/LookAround.cs
@@ -5,41 +5,32 @@ using UnityEngine;
 public class LookAround : MonoBehaviour
 {
     private bool _canLookAround = false;
-
-    public AnimationCurve animCurve;
-
-    private float afloat;
+    [SerializeField] private EnvirementInteraction envInteract;
 
     // Update is called once per frame
     void Update()
     {
-        SpeedAdjust();
-        //PlayerPrefs.SetString("_canLookSave", "CanLook");
-        //string IsSaveved = PlayerPrefs.GetString("_canLookSave");
-    }
-    public void SpeedAdjust()
-    {
-        
-
-        CanRotateCheck();
+        if (envInteract._isZoomedIn == false)
+        {
+            CanRotateCheck();
+        }
     }
 
     public void CanRotateCheck()
     {
-        afloat = Camera.main.transform.eulerAngles.y;
-        var result = (Input.mousePosition.x < Screen.width / 100 * 30 || Input.mousePosition.x > Screen.width / 100 * 70) ? ((Input.mousePosition.x > Screen.width / 100 * 70) ? (-50) : 50) :  (0);
+        var result = (Input.mousePosition.x < Screen.width / 2 && Input.GetKey(KeyCode.Mouse1) || Input.mousePosition.x > Screen.width / 2 && Input.GetKey(KeyCode.Mouse1)) ? ((Input.mousePosition.x >= Screen.width / 2) ? (-25) : 25) :  (0);
         lookAround(result);
     }
 
     public void lookAround(float rotationSpeed)
     {
-        
+        var afloat = Camera.main.transform.eulerAngles.y;
         if (Camera.main.transform.eulerAngles.y >= 340 || Camera.main.transform.eulerAngles.y <= 20)
         {
             Camera.main.transform.eulerAngles -= new Vector3(0, rotationSpeed * Time.deltaTime, 0);
         }
+
         var rotationY = (Camera.main.transform.eulerAngles.y !>= 340 || Camera.main.transform.eulerAngles.y !<= 20) ? (Camera.main.transform.eulerAngles.y) : (Camera.main.transform.eulerAngles.y <= 340 && Camera.main.transform.eulerAngles.y >= 160 ? 340.1f : 19.9f);
         Camera.main.transform.eulerAngles = new Vector3 (Camera.main.transform.eulerAngles.x, rotationY, Camera.main.transform.eulerAngles.z);
-        print(rotationSpeed);
     }
 }
