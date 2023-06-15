76e587f7a2262a39e6f879966c28f32cdab36b71
daan41114 <rendomgamer41114@gmail.com>
2023-06-15 14:58:47 +0200

    changed stuff in the scene

--- FakeNewsNetwork/Assets/Scripts/InvisibleSlider.cs
+++ FakeNewsNetwork/Assets/Scripts/InvisibleSlider.cs
@@ -1,10 +1,14 @@
 
+using System.Collections.Generic;
 using UnityEngine;
+
 
 
 public class InvisibleSlider : MonoBehaviour
 {
     [SerializeField] private int sliderValue;
+
+    public List<List<string>> reee = new List<List<string>>();
 
     private void SliderCalculator()
     {
