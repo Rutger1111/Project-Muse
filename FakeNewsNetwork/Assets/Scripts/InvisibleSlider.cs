1f56749fb3b181c810f5ef7675572b92c85a1b9d
daan41114 <rendomgamer41114@gmail.com>
2023-06-15 09:38:05 +0200

    made a script

--- FakeNewsNetwork/Assets/Scripts/InvisibleSlider.cs
+++ FakeNewsNetwork/Assets/Scripts/InvisibleSlider.cs
@@ -1,16 +1,12 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
-using UnityEngine.UIElements;
-using Slider = UnityEngine.UI.Slider;
+
 
 public class InvisibleSlider : MonoBehaviour
 {
     [SerializeField] private int sliderValue;
-    private void OnGUI()
+
+    private void SliderCalculator()
     {
-        sliderValue = Mathf.RoundToInt(GUI.HorizontalSlider(new Rect(), sliderValue, 0, 20));
-        //sliderValue = GUI.HorizontalSlider(new Rect(25, 25, 100, 30), sliderValue, 0.0f, 20f);
     }
 }
